41f6ed336ce3db50a678416095a68c5bacac0f26
PramudithaN <pramudithanadun@gmail.com>
2024-06-16 03:37:02 +0530

    SubMenu Collapsible

--- src/Components/Agent.tsx
+++ src/Components/Agent.tsx
@@ -1,19 +1,27 @@
 // AgentCard.tsx
-import React from 'react';
-import { Card } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import React from "react";
+import { Card } from "antd";
+import { UserOutlined } from "@ant-design/icons";
 
 interface AgentCardProps {
-    name: string;
+	name: string;
 }
 
 const AgentCard: React.FC<AgentCardProps> = ({ name }) => {
-    return (
-        <Card className="agent-card" style={{padding:'0px', backgroundColor: '#2a2a2a', color: '#fff' ,border:"none"}}>
-            <UserOutlined style={{ marginRight: '8px' }} />
-            {name}
-        </Card>
-    );
+	return (
+		<Card
+			className="agent-card"
+			style={{
+				padding: "8px",
+				backgroundColor: "#2a2a2a",
+				color: "#fff",
+				border: "none",
+			}}
+		>
+			<UserOutlined style={{ marginRight: "8px" }} />
+			{name}
+		</Card>
+	);
 };
 
 export default AgentCard;
